e2ce5a0fe872f4d105b10cbe7d0de614e2753b7a
Konrad Sladkowski <makabrak@gmail.com>
2017-12-06 23:22:27 +0100

    Add files via upload

---
+++ log1/index.php
@@ -0,0 +1,24 @@
+<! DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset = "utr-8"/>
+	<meta http-equiv ="X-UA-Compatible" content="IE=edge,chrome=1"/> 
+	<title>Sample Text Login</title>
+</head>
+
+
+<body>
+
+
+<form action = "login.php" method="post">
+
+		Login: <br/> <input type="text" name="login"/> <br/>
+		Hasło: <br/> <input type ="password" name = "pass"/> <br/> <br/>
+		<input type = "submit" value = "Zaloguj Się"/>
+		
+</form>
+
+
+
+</body>
+</html>
