2d9e18765bffa3ed06d35150365fb1daca4404e9
Gryfny <jan.snopek.86@gmail.com>
2018-01-14 15:44:26 +0100

    Cofka o jeden commit

--- cms/articles-options.php
+++ cms/articles-options.php
@@ -6,46 +6,33 @@ $articles = $article->fetch_all();
 session_start();
 ?>
 
-<html lang="pl">
+<html>
 <head>
     <title>CMS</title>
-    <link rel="stylesheet" href="../CSS/Login.css" /> 
-    <meta charset = "utr-8"/>
+    <link rel="stylesheet" href="admin-panel-style.css" /> 
 </head>
-    
 <body>
-    
-    <div id="art_nav">
-        <form>
-    <input type="button" value="Strona Główna" onclick="window.location.href='../index.php'" />
-        
-    <input type="button" value="Dodaj Artykuł" onclick="window.location.href='article-add.php'" />
-            
-    <input type="button" value="Panel Administratora" onclick="window.location.href='admin-panel.php'" />
-        </form>
-    </div>
-    
-    <div id="art_content">
-        <label>Baza artykułów</label>
-        <ol>
+    <div class="container">
+<a href="../index.php" id="logo">Strona glowna</a>
+</br>
+<a href="admin-panel.php" id="logo">CMS</a>
+</br>
+<a href="article-add.php" id="logo">Dodaj nowy artykuł</a>
+    <ol>
         <?php foreach($articles as $article) { ?>
         <li>
-            <input class="kneflik-gren"  type="button" value="Podgląd" onclick="window.location.href='article-edit.php?id=<?php echo $article['ArticleID']; ?>'" />
-        
-            <input class="kneflik-blue"  type="button" value="Edytuj" onclick="window.location.href='article-edit.php?id=<?php echo $article['ArticleID']; ?>'" />
-        
-            <input class="kneflik-red"  type="button" value="Usuń" onclick="window.location.href='article-delete.php?id=<?php echo $article['ArticleID']; ?>'" />
-            
+            <a href="article-edit.php?id=<?php echo $article['ArticleID']; ?>">
             <?php echo $article['Title']; ?>
-            
-            
-        - <small>
-        dodano <?php echo date("Y-m-d H:i:s",strtotime($article['InsertDate'])); ?>
-        </small>
-     
+            </a>
+            - <small>
+              dodano <?php echo date("Y-m-d H:i:s",strtotime($article['InsertDate'])); ?>
+            </small>
+            <a href="article-delete.php?id=<?php echo $article['ArticleID']; ?>">
+            Usuń</a>
+            <a href="article-edit.php?id=<?php echo $article['ArticleID']; ?>">
+             Edytuj</a>
         <?php } ?>
-        </ol>
-
+    </ol>
     </div>
 </body>
 </html>
