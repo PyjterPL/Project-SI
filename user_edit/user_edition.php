068d83d7477fd93bc20894a28b8686d9c48677e6
JanS270 <jan.snopek.86@gmail.com>
2018-01-17 21:38:49 +0100

    Edycja Profilu

--- user_edit/user_edition.php
+++ user_edit/user_edition.php
@@ -17,91 +17,116 @@ else $komm="";
 <html lang ="pl">
 <head> 
 
-    <label> <?php echo $name."    ".$komm; ?> </label>
-    </br>
-    <a href="../panel.php">Powrót</a>
+    
+    
+    
+    <link rel="stylesheet" href="../CSS/Login.css" /> 
 </head>
 
 <body>
-<br/> <br/>
+
    
-    <div>
-        <form action="user_name_edition.php" method="post" >
-            <label> Nowa Nazwa Użytkownika</label>
-            <input type = "text" name = "eName" >
-            <input type="submit" value = "Wyślij"/>
-            <?php // Sprawdzenie poprawoności loginu 
+<div id="edit-opt">
+
+        <label>Edycja użytkownika - <?php echo $name."    ".$komm; ?> </label>
+
+        <div id="rameczka">
+
+            <form action="user_name_edition.php" method="post" >
+
+                <label>Nazwa</label>
+                <input type = "text" name = "eName"  >
+
+                <input type="submit" value = "Wyślij"/>
+
+                <?php // Sprawdzenie poprawoności loginu 
+
                 if(isset($_SESSION['err_login']))
                 {
-                    echo '<div class="error">'.$_SESSION['err_login'].'</div>';
-                    unset($_SESSION['err_login']);
-                }
-            ?>
-    
-           
+                echo '<div class="error">'.$_SESSION['err_login'].'</div>';
+                unset($_SESSION['err_login']);
+                }?>
+                
             </form>
-     </div>
- 
-        <br/><br/>
-     <div>           
-    <form action="user_password_edition.php" method="post">
 
-        <label> Nowe Hasło Użytkownika</label>
-        <input type = "password" name = "ePass" > 
-           <br/> <br/>
-          ------------> Powtórz hasło  
-        <input type = "password" name = "ePassP" >
-        <input type="submit" value = "Wyślij"/>
-        <?php // Sprawdzenie poprawności haseł 
-			if(isset($_SESSION['err_haslo']))
-			{
-				echo '<div class="error">'.$_SESSION['err_haslo'].'</div>';
-				unset($_SESSION['err_haslo']);
-			}
-		?>
-         
-    </form>
+        </div>
+
+        <div id="rameczka">
+
+            <form action="user_password_edition.php" method="post">
+
+                <label>Hasło</label>
+                <input type = "password" name = "ePass" > </br>
+                <label>Powtórz hasło <input type = "password" name = "ePassP" ></label>
+                <input type="submit" value = "Wyślij"/> 
+                <?php // Sprawdzenie poprawności haseł 
+                if(isset($_SESSION['err_haslo']))
+                {
+                echo '<div class="error">'.$_SESSION['err_haslo'].'</div>';
+                unset($_SESSION['err_haslo']);
+                }
+                ?>
+
+            </form>
+        </div>
+
+
+        <div id="rameczka">
+            <form action ="user_email_edition.php" method="POST">
+
+                <label>E-Mail</label>
+                <input type = "tekst" name = "eEmail" >
+                <input type="submit" value = "Wyślij"/>
+                <?php // Sprawdzenie poprawności e-mail
+                if(isset($_SESSION['err_mail']))
+                {
+                echo '<div class="error">'.$_SESSION['err_mail'].'</div>';
+                unset($_SESSION['err_mail']);
+                }
+                ?>
+
+            </form>
+        </div>
+
+
+
+        <div id="rameczka">
+            <form action = "user_avatar_edition.php" method="POST"enctype="multipart/form-data">
+
+                <label>Avatar</label> 
+                <input type="file" name="eAvatar" accept="image/jpeg,image/gif,image/jpg" />
+                <input type="submit" value = "Wyślij"/>
+                <?php // Sprawdzenie poprawności e-mail
+                if(isset($_SESSION['err_avatar']))
+                {
+                echo '<div class="error">'.$_SESSION['err_avatar'].'</div>';
+                unset($_SESSION['err_avatar']);
+                }
+                ?>
+
+            </form>
+        </div>
+
+
+
+        <div id="rameczka">
+            <form action ="user_description_edition.php" method="post">
+
+                <label> Edytuj Opis </label>
+                <input type = "tekst" name = "eDescription" >
+                <input type="submit" value = "Wyślij"/>
+            </form>
+
+        </div>
+
+
+        
+            </br>
+
+            <div id="rameczka">
+        <input type="button" value="Strona główna" style="float: right"onclick="window.location.href='../index.php'" /> 
+        <input type="button" value="Powrót"  onclick="window.location.href='../panel.php'" /> 
+            </div>
     </div>
-
-    <br/><br/>
-    <form action ="user_email_edition.php" method="POST">
-
-        <label> Nowy E-Mail Użytkownika</label>
-        <input type = "tekst" name = "eEmail" >
-        <input type="submit" value = "Wyślij"/>
-        <?php // Sprawdzenie poprawności e-mail
-			if(isset($_SESSION['err_mail']))
-			{
-				echo '<div class="error">'.$_SESSION['err_mail'].'</div>';
-				unset($_SESSION['err_mail']);
-			}
-		 ?>
-        
-    </form>
-
-    <br/><br/>
-    <form action = "user_avatar_edition.php" method="POST"enctype="multipart/form-data">
-
-        <label> Zmien Avatar </label> 
-        <input type="file" name="eAvatar" accept="image/jpeg,image/gif,image/jpg" /></br></br>
-        <input type="submit" value = "Wyślij"/>
-        <?php // Sprawdzenie poprawności e-mail
-			if(isset($_SESSION['err_avatar']))
-			{
-				echo '<div class="error">'.$_SESSION['err_avatar'].'</div>';
-				unset($_SESSION['err_avatar']);
-			}
-		 ?>
-        
-    </form>
-    <br/> <br/>
-  
-    <form action ="user_description_edition.php" method="post">
-
-        <label> Edytuj Opis </label>
-        <input type = "tekst" name = "eDescription" >
-        <input type="submit" value = "Wyślij"/>
-    </form>
-    
 </body>
 </html>
