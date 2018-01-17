ae4365f476ccb371f783dab9b878801730de366b
JanS270 <jan.snopek.86@gmail.com>
2018-01-17 17:45:30 +0100

    edycja Artykułów

--- cms/article-edit.php
+++ cms/article-edit.php
@@ -14,42 +14,50 @@ if(isset($_GET['id']) && isset($_SESSION['logged']) && ($_SESSION['logged']==tru
     <html>
     <head>
         <title>Edycja artykułu</title>
-        <link rel="stylesheet" href="admin-panel-style.css" /> 
+        <link rel="stylesheet" href="../CSS/Login.css" /> 
     </head>
          <body>
-            <div class="container">
-            <a href="articles-options.php">Powrót</a>
-            </br>
-            <h4>Edycja artykułu</h4>
+            <div id="add_article">
+            <label>Edycja Artykułu</label>
                 <?php
                 if(isset($_GET['error']))
                 {
                     echo $_GET['error'];
-                    //echo '<img height="50" width="50" src="data:image/jpeg;base64,'.base64_encode( $_SESSION['Avatar'] ).'"/>';
                 }
                 ?>
 
                 <form action="edit-article-action.php?id=<?php echo $id; ?>" method="post" enctype="multipart/form-data">
-                <input type="text" name="title" placeholder="Tytuł" value="<?php echo $data['Title']; ?>"/></br></br>
-                <textarea rows="5" cols="20" placeholder="Wstęp" name="introduction"><?php echo $data['Introduction']; ?></textarea></br></br>
-                <textarea rows="15" cols="20" placeholder="Treść" name="content"><?php echo $data['Content']; ?></textarea></br></br>
-                <input type="text" name="tags" placeholder="Tagi" value="<?php echo $data['Tags']; ?>"/></br></br>
-                Obrazek <input type="file" name="obrazek" accept="image/jpeg,image/gif,image/jpg" /></br></br>
-              
-                <?php 
-                if(!empty($data['Image']))
-                {
-                 echo '<img height="50" width="50" src="data:image/jpeg;base64,'.base64_encode( $data['Image'] ).'"/>';
-                }
-                ?></br></br>
-                Kategoria <input list="categories" name = "category">
-                    <datalist id="categories">                                          
-                        <?php foreach($categories as $category) { ?> 
-                            <option value="<?php echo $category['CategoryID'].'.'.$category['Name']; ?>">  
-                            <?php } ?>                     
-                    </datalist> 
+                <div id="add-article-inputs">
+                    Tytuł: <input type="text" name="title" placeholder="Tytuł" value="<?php echo $data['Title']; ?>"/></br>
+                    Tagi: <input type="text" name="tags" placeholder="Tagi" value="<?php echo $data['Tags']; ?>"/></br>
+                    Kategoria: <input list="categories" name = "category" placeholder="Kliknij aby wybrać kategorię"></br>
+                    Obraz: <input type="file" name="obrazek" accept="image/jpeg,image/gif,image/jpg" />
+                    </div>
 
-                <input type="submit" value="Edytuj" />
+                    <div id="add-article-text">
+                    Wstęp:</br>
+                    <textarea rows="5" cols="40" placeholder="Wstęp" name="introduction"><?php echo $data['Introduction']; ?></textarea></br>
+                    Treść:</br>
+                    <textarea rows="15" cols="60" placeholder="Treść" name="content"><?php echo $data['Content']; ?></textarea>
+                    </div>
+
+                    <?php 
+
+                    if(!empty($data['Image']))
+                    {
+                    echo '<img height="50" width="50" src="data:image/jpeg;base64,'.base64_encode( $data['Image'] ).'"/>';
+                    }
+                    ?>
+                   
+                        <datalist id="categories">                                          
+                            <?php foreach($categories as $category) { ?> 
+                                <option value="<?php echo $category['CategoryID'].'.'.$category['Name']; ?>">  
+                                <?php } ?>                     
+                        </datalist> 
+
+                    <input type="submit" value="Edytuj" />
+                    <input type="button" value="Powrót" onclick="window.location.href='articles-options.php'" /> 
+
                 </form>
             </div>
         </body>
